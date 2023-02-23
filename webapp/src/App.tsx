013abc3ec85cc21e3249b01aba0265c9d92e45a2
uo282716 <uo282716@uniovi.es>
2023-02-23 17:35:27 +0100

    Implementación inicial del login

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,33 +5,35 @@ import Container from '@mui/material/Container';
 import EmailForm from './components/EmailForm';
 import Welcome from './components/Welcome';
 import UserList from './components/UserList';
-import  {getUsers} from './api/api';
+import {getUsers} from './api/api';
 import {User} from './shared/shareddtypes';
+import { SessionProvider, useSession } from "@inrupt/solid-ui-react";
 import './App.css';
+import LoginForm from './components/LoginForm';
+import ProfileViewer from './components/ProfileViewer';
 
-function App(): JSX.Element {
+const App = () => {
+  //We use this state variable
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const [users,setUsers] = useState<User[]>([]);
+  //With this we can control the login status for solid
+  const { session } = useSession();
 
-  const refreshUserList = async () => {
-    setUsers(await getUsers());
-  }
+  //We have logged in
+  session.onLogin(()=>{
+    setIsLoggedIn(true)
+  })
 
-  useEffect(()=>{
-    refreshUserList();
-  },[]);
+  //We have logged out
+  session.onLogout(()=>{
+    setIsLoggedIn(false)
+  })
 
-  return (
-    <>
-      <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
-        <UserList users={users}/>
-        <Link href="https://github.com/arquisoft/lomap_0">Source code</Link>
-      </Container>
-    </>
-  );
+  return(
+    <SessionProvider sessionId="log-in-example">
+      {(!isLoggedIn) ? <LoginForm/> : <ProfileViewer/>}
+    </SessionProvider>
+  )
 }
 
 export default App;
---
+++ webapp/src/components/ProfileViewer.tsx
@@ -0,0 +1,15 @@
+import { useSession, CombinedDataProvider, Image, LogoutButton, Text } from "@inrupt/solid-ui-react";
+import { Button, Card, CardActionArea, CardContent, Container, Typography } from "@mui/material";
+
+const ProfileViewer = () => {
+  const { session } = useSession();
+
+  return (
+    
+    
+    <p>Aquí iría la página de la aplicación (?</p>
+    
+  );
+}
+
+export default ProfileViewer
